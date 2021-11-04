01ade80039c56eb72477700368a16d946ac9a5df
Bhaumit <bpv6@njit.edu>
2021-11-03 22:29:38 -0400

    updated

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -1,7 +1,7 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 ?>
-<form onsubmit="return validate(this)" method="POST">
+<form onsubmit="return validate(this)" method="POST"> 
     <div>
         <label for="email">Email</label>
         <input type="email" name="email" required />
