e32e58281e48f88b221b978b59e82d2612eee4cb
Bhaumit <bpv6@njit.edu>
2021-11-12 14:20:01 -0500

    good

--- public_html/Project/logout.php
+++ public_html/Project/logout.php
@@ -3,7 +3,7 @@ session_start();
 session_unset();
 session_destroy();
 
-//setcookie("PHPESSID", "", time()-3600);
+//setcookies("PHPESSID", "", time()-3600);
 require_once(__DIR__ . "/../../partials/nav.php");
 flash("You have been logged out!", "success");
 die(header("Location: login.php")); 
