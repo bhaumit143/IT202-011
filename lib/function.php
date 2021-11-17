82335b11f33ab36680dcd218d85700966ff6425c
Bhaumit <bpv6@njit.edu>
2021-11-16 21:39:17 -0500

    made up to date

--- lib/function.php
+++ lib/function.php
@@ -1,29 +1,19 @@
 <?php
-
 require_once(__DIR__ . "/db.php");
 $BASE_PATH = '/Project/'; //This is going to be a helper for redirecting to our base project path since it's nested in another folder
 function se($v, $k = null, $default = "", $isEcho = true)
 {
-
-require_once(__DIR__ ."/db.php");
-
-function se($v, $k = null, $default = "", $isEcho = true) {
-
     if (is_array($v) && isset($k) && isset($v[$k])) {
         $returnValue = $v[$k];
     } else if (is_object($v) && isset($k) && isset($v->$k)) {
         $returnValue = $v->$k;
     } else {
         $returnValue = $v;
-
         //added 07-05-2021 to fix case where $k of $v isn't set
         //this is to kep htmlspecialchars happy
         if (is_array($returnValue) || is_object($returnValue)) {
             $returnValue = $default;
         }
-
-        
-
     }
     if (!isset($returnValue)) {
         $returnValue = $default;
@@ -36,7 +26,6 @@ function se($v, $k = null, $default = "", $isEcho = true) {
         return htmlspecialchars($returnValue, ENT_QUOTES);
     }
 }
-
 //TODO 2: filter helpers
 function sanitize_email($email = "")
 {
@@ -84,59 +73,21 @@ function get_user_email()
 function get_user_id()
 {
     if (is_logged_in()) { //we need to check for login first because "user" key may not exist
-=======
-function sanitize_email($email= ""){
-    return filter_var(trim($email), FILTER_SANITIZE_EMAIL);
-
-}
-function is_valid_email($email= ""){
-    return filter_var(trim($email), FILTER_VALIDATE_EMAIL);
-}
-//User Helper
-function is_logged_in(){
-    return isset($_SESSION["user"]); //se($_SESSION, "user", false, false);
-}
-function get_username(){
-    if (is_logged_in()){ //we need to check for login first because "user" key may not exist
-        return se($_SESSION["user"],"username", "", false);
-
-    }
-    return "";
-}
-function get_user_email(){
-    if (is_logged_in()){//we need to check for login first because "user" key may not exist
-        return se($_SESSION["user"], "email", "", false);
-
-    }
-    return "";
-}
-function get_user_id(){
-    if (is_logged_in()){//we need to check for login first because "user" key may not exist
-
         return se($_SESSION["user"], "id", false, false);
     }
     return false;
 }
-
 //TODO 4: Flash Message Helpers
 function flash($msg = "", $color = "info")
 {
     $message = ["text" => $msg, "color" => $color];
     if (isset($_SESSION['flash'])) {
-
-// falsh message system
-function flash($msg = "", $color="info")
-{
-    $message = ["text"=>$msg, "color"=>$color);
-    if (isset($_SESSION['flash'])){
-
         array_push($_SESSION['flash'], $message);
     } else {
         $_SESSION['flash'] = array();
         array_push($_SESSION['flash'], $message);
     }
 }
-
 
 function getMessages()
 {
@@ -168,7 +119,7 @@ function users_check_duplicate($errorInfo)
         //TODO come up with a nice error message
         flash("<pre>" . var_export($errorInfo, true) . "</pre>");
     }
-} 
+}
 function get_url($dest)
 {
     global $BASE_PATH;
@@ -179,23 +130,3 @@ function get_url($dest)
     //handle relative path
     return $BASE_PATH . $dest;
 }
-
-function getMessage(){
-    if (isset($_SESSION['flash'])) {
-        $flashes = $_SESSION['flash'];
-        $_SESSION['flash'] = array();
-        return $flashes;     
-    }
-    return array();
-}
-
-
-
-
-
-
-
-
-// end flash message system
-?> 
-
