ea84ec9575fcecf55488af8a60bceabaa0ac1c2e
Bhaumit <bpv6@njit.edu>
2021-11-22 22:08:57 -0500

    good

--- public_html/Project/myAccounts.php
+++ public_html/Project/myAccounts.php
@@ -1,8 +1,7 @@
-<?php require_once(__DIR__ . "/partials/nav.php");
-      require_once(__DIR__ . "/lib/helpers.php");
-?>
-
 <?php
+require(__DIR__ . "/../../partials/nav.php");
+?>
+<?php 
   $user = get_user_id();
   if(isset($user)){
   $results = [];
@@ -25,18 +24,18 @@
                 <div class="list-group-item">
                     <div>
                         <div><strong>Account Number:</strong></div>
-                        <div><?php safer_echo($r["account_number"]); ?></div>
+                        <div><?php flash($r["account_number"]); ?></div>
                     </div>
                     <div>
                         <div><strong>Account Type:</strong></div>
-                        <div><?php safer_echo($r["account_type"]); ?></div>
+                        <div><?php flash($r["account_type"]); ?></div>
                     </div>
                     <div>
                         <div><strong>Balance:</strong></div>
-                        <div><?php safer_echo($r["balance"]); ?></div>
+                        <div><?php flash($r["balance"]); ?></div>
                     </div>
                     <div>
-                        <a type="button" href="<?php echo get_URL("accounts/my_transactions.php?id=" . $r["AccID"]); ?>">View Transaction History</a>
+                        <a type="button" href="<?php url("accounts/my_transactions.php?id=" . $r["AccID"]); ?>">View Transaction History</a>
                     </div>
                 </div>
             <?php endforeach; ?>
@@ -45,4 +44,7 @@
         <p>No results</p>
     <?php endif; ?>
 </div>
-<?php require(__DIR__ . "/partials/flash.php");
+
+<?php
+require_once(__DIR__ . "/../../partials/flash.php");
+?>
