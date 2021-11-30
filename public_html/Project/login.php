de8303f9ff2a21ecba6713b2185534c272ab3b13
Bhaumit <bpv6@njit.edu>
2021-11-30 12:44:40 -0500

    updated

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -1,38 +1,50 @@
-<?php
 
+Login.php edited up to Milestone-1 
+
+<?php
 require(__DIR__ . "/../../partials/nav.php");
 ?>
-<form onsubmit="return validate(this)" method="POST">
+<form onsubmit="return validate(this)" method="POST"> 
+</style> 
+
     <div>
-
-        <label for="email"><br/>Username/Email</label><br/>
-        <input type="email" name="email" required />
-
-        <label for="email">Username/Email</label>
-        <input type="text" name="email" required />
-
-    </div>
+        <label for="email"><br/>Email</label>
+        <input type="email" name="email" required/><br/>
+        
+<style>
+form {
+width:100%;
+height:280px;
+font-size:20px;
+margin:auto;
+position:relative;
+}
+input {
+width:100%;
+size:1000px;
+height: 25px;
+}
+</style> 
+    
+</div>
     <div>
-        <label for="pw">Password</label><br/>
-        <input type="password" id="pw" name="password" required minlength="8" />
+        <label for="pw"><br/>Password</label>
+        <input type="password" id="pw" name="password" required minlength="8"/><br/>
+        
     </div><br/>
-    <input type="submit" value="Login" />
+    <br/><input type="submit" value="Login"/>
 </form>
 <script>
     function validate(form) {
-        //TODO 1: implement JavaScript validation
-
-        //ensure it returns false for an error and true for the success.
-=======
-        //ensure it returns false for an error and true for success
-
+        //TODO 1: implement JavaScript validations 
+        //ensure it returns false for an error and true for successs
 
         return true;
     }
 </script>
 <?php
 //TODO 2: add PHP Code
-if (isset($_POST["email"]) && isset($_POST["password"])) {
+if (isset($_POST["email"]) && isset($_POST["password"])) { 
     $email = se($_POST, "email", "", false);
     $password = se($_POST, "password", "", false);
 
@@ -42,19 +54,12 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         flash("Email must not be empty", "danger");
         $hasError = true;
     }
-    if (str_contains($email, "@")) {
-        //sanitize
-        $email = sanitize_email($email);
-        //validate
-        if (!is_valid_email($email)) {
-            flash("Invalid email address", "warning");
-            $hasError = true;
-        }
-    } else {
-        if (!preg_match('/^[a-z0-9_-]{3,30}$/i', $email)) {
-            flash("Username must only be alphanumeric and can only contain - or _", "warning");
-            $hasError = true;
-        }
+    //sanitize
+    $email = sanitize_email($email);
+    //validate
+    if (!is_valid_email($email)) {
+        flash("Invalid email address", "danger");
+        $hasError = true;
     }
     if (empty($password)) {
         flash("password must not be empty", "danger");
@@ -67,62 +72,40 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
     if (!$hasError) {
         //TODO 4
         $db = getDB();
-        $stmt = $db->prepare("SELECT id, email, username, password from Users where email = :email OR username = :email");
+        $stmt = $db->prepare("SELECT id, email, username, password from Users where email = :email");
         try {
             $r = $stmt->execute([":email" => $email]);
             if ($r) {
                 $user = $stmt->fetch(PDO::FETCH_ASSOC);
                 if ($user) {
-                    $hash = $user["password"];
+                    $hash = $user["password"]; 
                     unset($user["password"]);
                     if (password_verify($password, $hash)) {
                         flash("Welcome $email");
-
-                        $_SESSION["user"] = $user; 
-=======
                         $_SESSION["user"] = $user;
-                        //lookup potential roles
-                        $stmt = $db->prepare("SELECT Roles.name FROM Roles 
-                        JOIN UserRoles on Roles.id = UserRoles.role_id 
-                        where UserRoles.user_id = :user_id and Roles.is_active = 1 and UserRoles.is_active = 1");
-                        $stmt->execute([":user_id" => $user["id"]]);
-                        $roles = $stmt->fetchAll(PDO::FETCH_ASSOC); //fetch all since we'll want multiple
-                        //save roles or empty array
-                        if ($roles) {
-
-                            $_SESSION["user"]["roles"] = $roles; //at least 1 role
-=======
-                            $_SESSION["user"]["roles"] = $roles; //at least 1 roles.
-
-                        } else {
-                            $_SESSION["user"]["roles"] = []; //no roles
-                        }
-                        die(header("Location: home.php"));
+                        die(header("Location: home.php")); 
                     } else {
-                        flash("Invalid password", "danger");
-                    }
+                        flash("Invalid password", "danger"); 
+                    } 
                 } else {
                     flash("Email not found", "danger");
                 }
             }
-        } catch (Exception $e) {
-            flash("<pre>" . var_export($e, true) . "</pre>");
+        } catch (Exception $e) { 
+            flash("<pre>" . var_export($e, true) . "</pre>"); 
         }
     }
 }
 ?>
-<?php 
-require(__DIR__ . "/../../partials/flash.php");
 
+
+<?php
+require_once(__DIR__ . "/../../partials/flash.php");
 ?>
 
-=======
-?>
 
 <html>
-
 <head>
-
 </head>
 <body bgcolor="<?php
 if (isset($_POST['btn']))
@@ -164,101 +147,3 @@ echo $p="#ffffff";
 
 </body>
 </html>
-=======
-require_once(__DIR__ . "/../../partials/nav.php");
-if(isset($_POST["submit"])){
-    $email = se($_POST, "email", null, false);
-    $password = trim(se($_POST, "password", null, false));     
-    
-    $isValid = true;
-    if(!isset($email) || !isset($password)){
-        se("Must provide email, and password");
-        $isValid =false;
-    }
-    if (strlen($password) < 3) {
-        se("Password must be 3 or more characters");
-        $isValid = false; 
-    }    
-    $email = sanitize_email($email);
-    if(!is_valid_email($email)){
-        se("Invalid email");
-        $isValid = false;
-    }
-    if($isValid){
-        //do our registration
-        $db = getDB();
-        //$stmt = $db->prepare("INSERT INTO Users (email, password) VALUES (:email, :password)");
-        //$hash = password_hash($password, PASSWORD_BCRYPT); 
-        $stmt = $db->prepare("SELECT id, email, password from Users where email = :email LIMIT 1");
-        try {
-            $stmt->execute([":email" => $email]);
-            $user = $stmt->fetch(PDO::FETCH_ASSOC);
-            if($user){
-                $upass = $user["password"];
-                if(password_verify($password, $upass)){
-                    se("Yay we logged in");
-                    unset($user["password"]);
-                    $_SESSION["user"] = $user;
-                    echo "<pre>" .var_export($_SESSION, true) . "</pre>";
-                    die(header("Location: home.php"));
-                } else {
-                    se("Password don't match");
-
-                }
-            } else {
-                se("User doesn't exist");
-            }
-        } catch(Exception $e) {
-            $code = se($e->errorInfo, 0, "00000", false);
-            if ($code === "23000") {
-                se("An account with this email already exists");
-            } else {
-                echo "<pre>" . var_export($e->errorInfo, true) . "</pre>";
-            }    
-        
-        
-        }    
-    } 
-}
-?>
-<div> 
-    <h1>Login</h1>
-    <form method="POST" onsubmit="return validate(this);">
-        <div>
-            <lable for="email">Email: </lable>
-            <input type="email" id="email" name="email" required />
-        </div>
-        <div>
-            <lable for="pw">Password: </lable>
-            <input type="password" id="pw" name="password" required />
-        </div>
-        <div>
-            <input type="submit" name="submit" value="Login" />
-        </div>
-    </form>
-<div>
-<script>
-    function validate(form){
-        let email = form.email.value;
-        let password = form.password.value;
-        let isValid = true; 
-        if (email){
-            email = email.trim();
-        }
-        if(password){
-            password = password.trim();
-        }
-        
-        if(email.indexOf("@") === -1){
-            isValid = false;
-            alert("Invalid email");
-        }
-        if(password.length < 3){
-            isValid = false;
-            alert("password must be 3 or more characters");     
-        }
-        return isValid;
-    }
-
-</script> 
-
--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -1,114 +1,27 @@
+Register.php edited up to Milestone-1
+
 <?php
-
- Feat-UserRegistration
-require_once(__DIR__ . "/../../partials/nav.php"); //flash
-if(isset($_POST["submit"])){
-    $email = se($_POST, "email", null, false); 
-    $password = trim(se($_POST, "password", null, false));   
-    $confirm = trim(se($_POST, "confirm", null, false)); 
-    $username = trim(se($_POST, "username", null, false)); 
-    
-    $isValid = true; 
-    if(!isset($email) || !isset($password) || !isset($confirm) || !isset($username))  {
-        se("Must provide email, password, and confirm password"); //flash
-        $isValid =false; 
-    }   
-    if ($password !== $confirm){  
-        se("Passwords don't match"); //flash
-        $isValid = false;  
-    } 
-    if (strlen($password) < 3) {
-        se("Password must be 3 or more characters"); //flash
-        $isValid = false; 
-    }   
-    $email = sanitize_email($email);
-    if(!is_valid_email($email)){
-        se("Invalid email"); //flash
-        $isValid = false;
-    }
-    //TODO add validation for username (length? disallow special chars? etc)
-=======
-require_once(__DIR__ . "/../../partials/nav.php");
-
-if(isset($_POST["submit"])){
-    $email = se($_POST, "email", null, false);
-    $password = trim(se($_POST, "password", null, false));   
-    $confirm = trim(se($_POST, "confirm", null, false));
-    
-    $isValid = true;
-    if(!isset($email) || !isset($password) || !isset($confirm)){
-        se("Must provide email, password, and confirm password");
-        $isValid =false;
-
-    }
-
-    if ($password !== $confirm){
-        se("Passwords don't match");
-        $isValid = false;
-    } 
-    if (strlen($password) < 3) {
-        se("Password must be 3 or more characters");
-        $isValid = false; 
-    }
-    
-    $email = sanitize_email($email);
-    if(!is_valid_email($email)){
-        se("Invalid email");
-        $isValid = false;
-    }
-
-    if($isValid){
-        //do our registration
-        $db = getDB();
-        $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES (:email, :password)");
-        $hash = password_hash($password, PASSWORD_BCRYPT);
-        try {
-
-            $stmt->execute([":email" => $email, ":password" => $hash]);
-        } catch(PDOException $e) {
-            $code = se($e->errorInfo, 0, "00000", false);
-            if ($code === "23000") {
-
-                se("An account with this email already exists"); //flash
-            } else {
-                echo "<pre>" . var_export($e->errorInfo, true) . "</pre>";
-            }    
-         
-=======
-                se("An account with this email already exists");
-            } else {
-                echo "<pre>" . var_export($e->errorInfo, true) . "</pre>";
-            }    
-        
-
-        
-        }    
-    } 
-}
-
-
-
 require(__DIR__ . "/../../partials/nav.php"); 
- Milestone1
 ?>
 <form onsubmit="return validate(this)" method="POST">
-    <div><br/>
-        <label for="email">Email: </label><br/>
+    <div>
+        <label for="email"><br/>Email: </label>
         <input type="email" id = "email" name="email" required />
     </div> 
     <div> 
-        <label for="username">Username: </label><br/>
+        <label for="username"><br/>Username: </label>
         <input type="text" id = "username" name="username" required maxlength="30" />
     </div>
     <div>
-        <label for="pw">Password: </label><br/>
+        <label for="pw"><br/>Password: </label>
         <input type="password" id="pw" name="password" required minlength="8" />
     </div> 
     <div>
-        <label for="cpw">Confirm Password: </label><br/>
+        <label for="cpw"><br/>Confirm Password: </label>
         <input type="password" id ="cpw" name="confirm" required minlength="8" />
     </div> <br/>
     <input type="submit" name = "submit" value="Register" />
+    
 </form>
 <script>
     function validate(form) { 
@@ -138,76 +51,11 @@ require(__DIR__ . "/../../partials/nav.php");
             isValid = false;
             alert("Invalid email");
         }
- Feat-UserRegistration
-        if(password != confirm){
-            isValid = false; 
-            alert("password don't match"); 
-        }
-        if(password.length < 3){
-            isValid = false;
-            alert("password must be 3 or more characters");      
-=======
-?>
-<div> 
-    <h1>Register</h1>
-    <form method="POST" onsubmit="return validate(this);">
-        <div>
-            <lable for="email">Email: </lable>
-            <input type="email" id="email" name="email" required />
-        </div>
-        <div>
-            <lable for="pw">Password: </lable>
-            <input type="password" id="pw" name="password" required />
-        </div>
-        <div>
-            <lable for="cpw">Confirm Password: </lable>
-            <input type="password" id="cpw" name="confirm" required />   
-        </div>
-        <div>
-            <input type="submit" name="submit" value="Register" />
-        </div>
-    </form>
-<div>
-<script>
-    function validate(form){
-        let email = form.email.value;
-        let password = form.password.value;
-        let confirm = form.confirm.value;
-        let isValid = true; 
-        if (email){
-            email = email.trim();
-        }
-        if(password){
-            password = password.trim();
-        }
-        if(confirm ){
-            confirm = confirm.trim();
-        }
-        if(email.indexOf("@") === -1){
-            isValid = false;
-            alert("Invalid email");
-        }
-        if(password != confirm){
-            isValid = false; 
-            alert("password don't match");
-        }
-        if(password.length < 3){
-            isValid = false;
-            alert("password must be 3 or more characters");     
-
-        }
-        return isValid;
-    }
-
-
-</script>  
-
-
         //TODO 1: implement JavaScript validation
         //ensure it returns false for an error and true for success
- Milestone1
 
         return true;
+        
     }
 </script>
 <?php
@@ -271,6 +119,20 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
 require_once(__DIR__ . "/../../partials/flash.php");
 ?>
 
+<style>
+form {
+width:100%;
+height:300px;
+font-size:15px;
+margin:auto;
+position:relative;
+}
+input {
+width:100%;
+size:2000px;
+height: 23px;
+}
+</style> 
 
 <html>
 
@@ -310,14 +172,9 @@ echo $p="#ffffff";
 <option value="#ffffff"> White </option>
 <option value="#ffff00"> Yellow </option>
 </select>
-<br><br/>
+<br>
+<br/>
 <input type="submit" name="btn" value="Submit">
 </form>
-
 </body>
 </html>
-=======
-</script>
-
-
-
