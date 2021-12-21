17ab988f79c977246e2d706d175cdff8998970f3
Bhaumit <bpv6@njit.edu>
2021-12-21 16:40:54 -0500

    case secsitive chane

--- partials/nav.php
+++ partials/nav.php
@@ -6,7 +6,7 @@ if(strpos($domain, ":")){
 }
 session_set_cookie_params([
     "lifetime" => 60 * 60, 
-    "path" => "/project",
+    "path" => "/Project",
     //"domain" => $_SERVER["HTTP_HOST"],
     "domain"=> $domain,
     "secure" => true,
